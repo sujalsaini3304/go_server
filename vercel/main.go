e3d61b1c2b5d70e871baf2645d6cf43e40d68e43
Sujal Kumar Saini <sujalsaini3304@gmail.com>
2025-02-23 18:32:22 +0530

    modified

--- vercel/main.go
+++ vercel/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/sujalsaini3304/go_server/routes"
 )
 
-func RequestHandler(w http.ResponseWriter, r *http.Request) {
+func Handler(w http.ResponseWriter, r *http.Request) {
 	router := gin.Default()
 	routes.RegisterRoutes(router)
 	router.ServeHTTP(w, r)
